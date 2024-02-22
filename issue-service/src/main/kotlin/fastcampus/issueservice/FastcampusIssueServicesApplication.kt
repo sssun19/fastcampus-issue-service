12af45cf7c6a58ad9a2d02d9378b9f22a09c910a
sssun <98_0509@naver.com>
2024-02-22 23:53:45 +0900

    webConfig

---
+++ issue-service/src/main/kotlin/fastcampus/issueservice/FastcampusIssueServicesApplication.kt
@@ -0,0 +1,11 @@
+package fastcampus.issueservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FastcampusIssueServicesApplication
+
+fun main(args: Array<String>) {
+	runApplication<FastcampusIssueServicesApplication>(*args)
+}
