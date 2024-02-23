e8daccdd9bd7f7f79db889c31985e45c82e2d05c
sssun <98_0509@naver.com>
2024-02-24 00:06:31 +0900

    Issue enum

--- issue-service/src/main/kotlin/fastcampus/issueservice/domain/enums/IssuePriority.kt
+++ issue-service/src/main/kotlin/fastcampus/issueservice/domain/enums/IssuePriority.kt
@@ -2,4 +2,11 @@ package fastcampus.issueservice.domain.enums
 
 enum class IssuePriority {
 
+    LOW, MEDIUM, HIGH;
+
+    companion object {
+        operator fun invoke(priority: String) = valueOf(priority.uppercase())
+    }
+
+
 }
