bc29650ffa10262fba126ebf840e8fbca8bfb313
sssun <98_0509@naver.com>
2024-02-24 00:19:20 +0900

    service

---
+++ issue-service/src/main/kotlin/fastcampus/issueservice/service/IssueService.kt
@@ -0,0 +1,28 @@
+package fastcampus.issueservice.service
+
+import fastcampus.issueservice.domain.Issue
+import fastcampus.issueservice.domain.IssueRepository
+import fastcampus.issueservice.model.IssueRequest
+import fastcampus.issueservice.model.IssueResponse
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class IssueService(
+    private val issueRepository: IssueRepository,
+) {
+
+    @Transactional
+    fun create(userId : Long, request: IssueRequest) : IssueResponse {
+
+        val issue = Issue(
+            summary = request.summary,
+            description = request.description,
+            userId = userId,
+            type = request.type,
+            priority = request.priority,
+            status = request.status,
+        )
+        return IssueResponse(issueRepository.save(issue))
+    }
+}
